9e1715c376c27c09a46eeb0daff3e918df688de4
Francisco Vasquez <francisco.vasquez@technifiser.com>
2016-07-31 02:13:08 -0400

    ToListAsync with paging implemented

--- app/IngswDev/EntityFramework/Repository/Repository.cs
+++ app/IngswDev/EntityFramework/Repository/Repository.cs
@@ -5,12 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace IngswDev.EntityFramework.Repository
 {
     public abstract class Repository<TEntity> : IRepository<TEntity>
         where TEntity : class
     {
+        protected const int DEFAULT_PAGE_SIZE = 20;
+
         protected readonly IngswDevDB _db;
         protected readonly ILogger<Repository<TEntity>> _logger;
 
@@ -20,41 +23,49 @@ namespace IngswDev.EntityFramework.Repository
             _logger = logger;
         }
 
-        public void Add(TEntity entity)
+        public virtual void Add(TEntity entity)
         {
             _db.Set<TEntity>().Add(entity);
             _db.Entry(entity).State = EntityState.Added;
             _logger?.LogInformation($"New instance pf {typeof(TEntity).Name} was added");
         }
 
-        public void Delete(TEntity entity)
+        public virtual void Delete(TEntity entity)
         {
             _db.Set<TEntity>().Attach(entity);
             _db.Entry(entity).State = EntityState.Deleted;
             _logger?.LogInformation($"New instance pf {typeof(TEntity).Name} was marked to delete");
         }
 
-        public Task<int> SaveAsync()
+        public virtual Task<int> SaveAsync()
         {
             return _db.SaveChangesAsync();
         }
 
-        public Task<TEntity> SelectAsync(Expression<Func<TEntity, bool>> predicate)
+        public virtual Task<TEntity> SelectAsync(Expression<Func<TEntity, bool>> predicate)
         {
             return _db.Set<TEntity>().FirstOrDefaultAsync(predicate);
         }
 
-        public Task<List<TEntity>> ToListAsync()
+        public virtual Task<List<TEntity>> ToListAsync()
         {
             return _db.Set<TEntity>().ToListAsync();
         }
 
-        public Task<List<TEntity>> ToListAsync(Expression<Func<TEntity, bool>> predicate)
+        public virtual Task<List<TEntity>> ToListAsync(Expression<Func<TEntity, bool>> predicate, int? page, int? pageSize)
+        {
+            pageSize = pageSize ?? DEFAULT_PAGE_SIZE;
+            page = page ?? 0;
+            return _db.Set<TEntity>().Where(predicate).Skip((int)(page * pageSize))
+                .Take((int)pageSize).ToListAsync();
+        }
+
+        public virtual Task<List<TEntity>> ToListAsync(Expression<Func<TEntity, bool>> predicate)
         {
             return _db.Set<TEntity>().Where(predicate)
                 .ToListAsync();
         }
-        public void Update(TEntity entity)
+        public virtual void Update(TEntity entity)
         {
             _db.Set<TEntity>().Attach(entity);
             _db.Entry(entity).State = EntityState.Modified;
