3d4d449fcf565672bbef2e980c6a5aff8e94d3fb
Francisco Vasquez <francisco.vasquez@technifiser.com>
2016-07-31 01:12:36 -0400

    Db Transaction Filter Implementation

---
+++ app/IngswDev/Filters/TransactionalFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using IngswDev.EntityFramework;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace IngswDev.Filters
+{
+    public class TransactionalFilterAttribute : ActionFilterAttribute
+    {
+        private readonly IngswDevDB _db;
+
+        public TransactionalFilterAttribute(IngswDevDB db)
+        {
+            _db = db;
+        }
+
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            using (var transaction = await _db.Database.BeginTransactionAsync())
+            {
+                var executedContext = await next();
+                if (executedContext.Exception != null)
+                    transaction.Rollback();
+                else
+                    transaction.Commit();
+            }
+        }
+    }
+}
